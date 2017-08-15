fdaa2d44c321375b8696bae97dcb729e4a2ccc67
shritejmayekar <shritejmayekar>
2017-08-15 20:52:24 +0530

    programs corrected

--- com/Bridgelabz/util/Sin.java
+++ com/Bridgelabz/util/Sin.java
@@ -13,20 +13,55 @@
  import java.util.Scanner;
  
 public class Sin {
-	static double x,n;
-	/*
-	*	main method to calculate sine talyors series
-	*/
+	//variable declaration
+    static double fact=1,sum=0,number=8,i=1;
+    static double counter=2,angleDegree,sumSinx;
+    static double angleRadian;
+	//method to find factorial
+    public static double factorial(double number)
+    {
+       while(number>0)
+       {
+           fact=fact*number;
+           number=number-1;
+       }
+       return fact;
+    }
+	//method to displaySeries
+	public static void displaySeries(double angleRadian )
+	{
+		while(i<number)
+        {
+            System.out.print(angleRadian+"^"+i+"/");
+            System.out.print(i);
+            if(counter++%2==0)
+                System.out.print("-");
+            else
+                System.out.print("+");
+            
+            i=i+2;
+        }
+	}
+    //main method to get sinx 
     public static void main(String[] args) {
-		//take angle in degree
-		Scanner in=new Scanner(System.in);
-		x=in.nextDouble();
-		//convert degree
-		x=x%(Math.PI*2);
-		
-		
-		
-	
+            
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the angle:");
+        angleDegree=input.nextInt();
+       //angle to radian
+        angleRadian=angleDegree * (Math.PI/180);
+        displaySeries(angleRadian);
+        i=1;
+        int sign=1;
+        while(i<number)
+        {
+            sum+=Math.pow(angleRadian, i)/factorial(i);
+            sign=-sign;
+            i=i+2;
+        }
+        System.out.println("\nsinx="+sum);
+        
     }
     
 }
+

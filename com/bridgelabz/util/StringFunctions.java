a45f34c5c154b16e5880f17745e057d9e8bf6670
shritejmayekar <shritejmayekar>
2017-09-03 15:13:48 +0530

    method added

--- com/bridgelabz/util/StringFunctions.java
+++ com/bridgelabz/util/StringFunctions.java
@@ -12,6 +12,7 @@
 package com.bridgelabz.util;
 
 import java.util.Scanner;
+
 /**
 *	Main Class to perform string functions
 */
@@ -41,6 +42,7 @@ public class StringFunctions {
 */
     public static Boolean isPallindrome(String inputname)
 	{
+		inputname=inputname.toLowerCase();
 		name=inputname.toCharArray();
 		for(int i=0;i<name.length/2;i++)
 		{
@@ -60,6 +62,7 @@ public class StringFunctions {
 */    
     public static Boolean isAnagram(String name1,String name2 ) 
     {
+		
         name=name1.toCharArray();
         dummyname=name2.toCharArray();
         if(name1.length()==name2.length())
@@ -83,6 +86,81 @@ public class StringFunctions {
 		return false;  
     }
 /**
+*	static method to check two string  are anagram or not
+*/
+	public static boolean isAnagram2(String name1,String name2)
+	{
+		name=name1.toCharArray();
+		dummyname=name2.toCharArray();
+		name=sortChar(name);
+		dummyname=sortChar(dummyname);
+		String string1=new String(name);
+		String string2=new String(dummyname);
+		if(string1.equals(string2))
+			return true;
+		return false;
+	}
+	
+/**
+*	Sort char
+*/
+	public static char[] sortChar(char[] name)
+	{
+		
+		for(int i=0;i<name.length;i++)
+		{
+			for(int j=0;j<name.length;j++)
+			{
+				if(name[i]<name[j])
+				{
+					char temp=name[j];
+					name[j]=name[i];
+					name[i]=temp;
+				}
+			}
+		}
+		// for(int i=0;i<name.length;i++)
+			// System.out.println(name[i]);
+		return name;
+	}
+	public static void recuString(String name)
+	{
+		dummyname=name.toCharArray();
+		char name2[];
+		for(int i=0;i<dummyname.length;i++)
+		{
+			char c=dummyname[0];
+			dummyname[0]=dummyname[i];
+			dummyname[i]=c;
+			
+			name2=dummyname;
+			int k=1;
+			//System.out.println(name2);
+			for(int j=0;j<dummyname.length-1;j++)
+			 {
+				 System.out.println(name2);
+				 char temp;
+			temp=name2[name2.length-1];
+			 name2[name2.length-1]=name2[j];
+			 name2[j]=temp;
+			 
+			//System.out.println(name2);
+			// if(k<3)
+			// {
+			// char temp;
+			// temp=name2[k];
+			// name2[k]=name2[k+1];
+			// name2[k]=temp;
+			// //System.out.println(name2);
+			// k++;
+			// }
+			
+			}
+			
+		}
+	}
+	
+/**
 *	main method to perform string functions
 */
     public static void main(String[] args) {
@@ -91,7 +169,7 @@ public class StringFunctions {
 */       
 	Scanner input=new Scanner(System.in);
 	System.out.println("Please enter the choice\n"
-               + " 1.Anagram \n 2.Pallindrome ");
+               + " 1.Anagram \n 2.Pallindrome \n 3.Recur");
     choice=input.nextInt();	//Take choice of above Menu
         switch(choice)
         {
@@ -120,6 +198,12 @@ public class StringFunctions {
             else
                 System.out.println("The "+inputName+NOT_PALLINDROME_MESSAGE);
                 break;
+			case 3:
+			System.out.print("Please enter the Name:");
+            inputName=input.next();
+			recuString(inputName);
+			
+			break;
            
            default:
                System.out.print("Inavalid");
